12de1665919c5065fb013c974a3ee3abda47798b
delkabo <delkabo@yandex.ru>
2022-05-11 14:40:13 +0300

    added baseurl in DriverSettings.java

--- src/test/java/com/delkabo/tests/LeroyMerlin.java
+++ src/test/java/com/delkabo/tests/LeroyMerlin.java
@@ -4,13 +4,17 @@ import com.codeborne.selenide.SelenideElement;
 import com.delkabo.components.LeroyMerlinObject;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Selenide.open;
+
 public class LeroyMerlin extends TestBase{
 
     LeroyMerlinObject leroyMerlinObject = new LeroyMerlinObject();
 
     @Test
     void leroyMerlin() {
-        leroyMerlinObject.openPage()
+        open("https://hh.ru/");
+
+        leroyMerlinObject
                 .searchVacancy("Леруа Мерлен QA Automation")
                 .clickSearched("QA Automation")
                 .shouldTitle("Леруа Мерлен");
